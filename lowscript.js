421e0e95688d4de2f9bbf1a93d25342e35a10104
georgolden <georgoldenb@gmail.com>
2022-04-25 22:45:04 +0200

    refactor: parse line independently to support potential new steps by adding one line

--- lowscript.js
+++ lowscript.js
@@ -2,38 +2,46 @@
 
 const fsp = require('fs').promises;
 
-const loadFlow = async (fileName) => {
-  const buffer = await fsp.readFile(fileName);
-  return buffer.toString();
-};
+const loadFlow = (fileName) => fsp.readFile(fileName, { encoding: 'utf8' });
 
 const notEmpty = (s) => s.length > 0;
 
 const cutBullet = (s, len = 1) => s.substring(len).trim();
+
+const steps = [
+  /* step , prefix */
+  ['command', '*'],
+  ['finalization', '- >'],
+  ['fail', '-'],
+  ['success', '+'],
+  // new steps can be added here
+];
+
+const parseLine = (line) => {
+  const str = line.trim();
+  const [step, prefix] = steps.find((cond) => str.startsWith(cond[1]));
+  return { step, parsed: cutBullet(str, prefix.length) };
+};
 
 const parseProc = (script) => {
   const pos = script.indexOf('\n');
   const name = script.substring(0, pos);
   const lines = script.substring(pos).trim().split('\n');
   const body = [];
-  let step = null;
   for (const line of lines) {
-    if (line.startsWith('*')) {
-      if (step) body.push(step);
-      step = {
-        command: cutBullet(line),
+    const { step, parsed } = parseLine(line);
+    if (step === 'command') {
+      body.push({
+        command: parsed,
         success: [],
         fail: [],
         finalization: [],
-      };
+      });
     } else {
-      const str = line.trim();
-      if (str.startsWith('- >')) step.finalization.push(cutBullet(str, 3));
-      else if (str.startsWith('-')) step.fail.push(cutBullet(str));
-      else if (str.startsWith('+')) step.success.push(cutBullet(str));
+      const last = body[body.length - 1];
+      last[step].push(parsed);
     }
   }
-  if (step) body.push(step);
   return { name, body };
 };
 
