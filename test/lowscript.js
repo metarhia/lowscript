e4d15ffb475873af916284bcf754aac0e3d70029
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-04-26 19:02:23 +0300

    Remove node:fs from lowscript dependencies

--- test/lowscript.js
+++ test/lowscript.js
@@ -1,16 +1,17 @@
 'use strict';
 
+const fsp = require('fs').promises;
 const metatests = require('metatests');
 const lowscript = require('..');
 
 metatests.test('Lowscript parsing example', async (test) => {
-  const filePath = `./test/examples/Order product.md`;
-  const src = await lowscript.loadFlow(filePath);
-  const script = lowscript.parseScript(src);
-  test.strictSame(script[0].name, 'Order product');
-  test.strictSame(script[0].body[0].command, 'Form `Order`');
-  test.strictSame(script[0].body[1].fail.length, 1);
-  test.strictSame(script[0].body[3].success.length, 1);
-  test.strictSame(script[0].body[3].finalization.length, 1);
+  const fileName = `./test/examples/Order product.md`;
+  const src = await fsp.readFile(fileName, 'utf8');
+  const bp = lowscript.parseProcess(src);
+  test.strictSame(bp[0].name, 'Order product');
+  test.strictSame(bp[0].body[0].command, 'Form `Order`');
+  test.strictSame(bp[0].body[1].fail.length, 1);
+  test.strictSame(bp[0].body[3].success.length, 1);
+  test.strictSame(bp[0].body[3].finalization.length, 1);
   test.end();
 });
