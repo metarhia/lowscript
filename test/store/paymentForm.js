1218ffadeb0db0cc28501c1c8e916319519862ee
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-04-28 19:03:14 +0300

    Fix context for forms: include main process context

--- test/store/paymentForm.js
+++ test/store/paymentForm.js
@@ -1,8 +1,8 @@
-async ({ paymentForm }) => {
+async ({ order, paymentForm }) => {
   const { amount, transaction } = paymentForm;
   const payment = {
     paymentId: 1,
-    orderId: 1,
+    orderId: order.orderId,
     amount,
     transaction,
     date: new Date(),
